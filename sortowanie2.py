6b70b51fabb53b1ea5177474a9600e60cb42eb7d
rudolf <k.j.malazdra@gmail.com>
2021-02-16 22:29:12 +0100

    Dodany heapsort

--- sortowanie2.py
+++ sortowanie2.py
@@ -1,5 +1,4 @@
 import random
-import os
 from datetime import datetime
 
 
@@ -21,22 +20,23 @@ def quickSortLast1(input_list, low, high):
         quickSortLast1(input_list, partition_index + 1, high)
 
 
-def partitionLast2(input_list, low, high):
-    i = (low - 1)
-    pivot = input_list[high]
-    for j in range(low, high):
-        if input_list[j] >= pivot:
+def partitionLast2(input_list1, low1, high1):
+    i = (low1 - 1)
+    pivot = input_list1[high1]
+    for j in range(low1, high1):
+        if input_list1[j] >= pivot:
             i = i + 1
-            input_list[i], input_list[j] = input_list[j], input_list[i]
-    input_list[i + 1], input_list[high] = input_list[high], input_list[i + 1]
+            input_list1[i], input_list1[j] = input_list1[j], input_list1[i]
+    input_list1[i + 1], input_list1[high1] = input_list1[high1], input_list1[i + 1]
     return (i + 1)
 
 
-def quickSortLast2(input_list, low, high):
-    if low < high:
-        partition_index = partitionLast2(input_list, low, high)
-        quickSortLast2(input_list, low, partition_index - 1)
-        quickSortLast2(input_list, partition_index + 1, high)
+def quickSortLast2(input_list1, low1, high1):
+    if low1 < high1:
+        partition_index1 = partitionLast2(input_list1, low1, high1)
+        quickSortLast2(input_list1, low1, partition_index1 - 1)
+        quickSortLast2(input_list1, partition_index1 + 1, high1)
+
 
 # mid pivot
 
@@ -58,6 +58,7 @@ def quickSortMid1(input_list, low, high):
         quickSortMid1(input_list, low, pivot - 1)
         quickSortMid1(input_list, pivot + 1, high)
 
+
 def partitionMid2(input_list, low, high):
     input_list[low], input_list[(low + high) // 2] = input_list[(low + high) // 2], input_list[low]
     pivot = input_list[low]
@@ -75,6 +76,7 @@ def quickSortMid2(input_list, low, high):
         pivot = partitionMid2(input_list, low, high)
         quickSortMid2(input_list, low, pivot - 1)
         quickSortMid2(input_list, pivot + 1, high)
+
 
 # losowy pivot
 
@@ -98,7 +100,6 @@ def quickSortRand1(input_list, low, high):
         quickSortRand1(input_list, pivot + 1, high)
 
 
-
 def partitionRand2(input_list, low, high):
     randomNum = random.randint(low, high)
     input_list[low], input_list[randomNum] = input_list[randomNum], input_list[low]
@@ -113,48 +114,171 @@ def partitionRand2(input_list, low, high):
 
 
 def quickSortRand2(input_list, low, high):
-    if low < high:
+    if low > high:
         pivot = partitionRand2(input_list, low, high)
         quickSortRand2(input_list, low, pivot - 1)
         quickSortRand2(input_list, pivot + 1, high)
 
 
-tablica = [random.randint(-10000, 10000) for i in range(1000000)]  # Tutaj w eksperymencie następywało manualne zmienianie ilości elementów w zależności od badanego elementu pomiarowego
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[largest] < arr[l]:
+        largest = l
+
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+
+        heapify(arr, n, largest)
+
+
+def heapSort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+
+def heapify2(arr, n, i):
+    smallest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[l] < arr[smallest]:
+        smallest = l
+
+    if r < n and arr[r] < arr[smallest]:
+        smallest = r
+
+    if smallest != i:
+        (arr[i],
+         arr[smallest]) = (arr[smallest],
+                           arr[i])
+
+        heapify2(arr, n, smallest)
+
+
+def heapSort2(arr, n):
+    for i in range(int(n / 2) - 1, -1, -1):
+        heapify2(arr, n, i)
+
+    for i in range(n - 1, -1, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+
+        heapify2(arr, i, 0)
+
+
+tablica = [random.randint(-10000, 10000) for i in range(1000)]
 tablica1 = list(tablica)
 tablica2 = list(tablica)
 tablica3 = list(tablica)
+tablica4 = list(tablica)
 dl = len(tablica)
+n = len(tablica)
 
+print("\n")
+print("Quick Sort z wskaźnikiem na końcu")
+
+print("sortowanie")
 t1 = datetime.now()
 quickSortLast1(tablica1, 0, dl - 1)
-print(tablica1)
-quickSortLast2(tablica1, 0, dl - 1)
-print(tablica1)
 t2 = datetime.now()
 t3 = t2 - t1
-# print(tablica1)
 print(t3)
 
+print("sortowanie")
+t1 = datetime.now()
+quickSortLast2(tablica1, 0, dl - 1)
+t2 = datetime.now()
+t3 = t2 - t1
+print(t3)
 
-
+print("sortowanie odwrotne")
 t1 = datetime.now()
 quickSortMid1(tablica2, 0, dl - 1)
-print(tablica2)
-quickSortMid2(tablica2, 0, dl - 1)
-print(tablica2)
+t2 = datetime.now()
+t3 = t2 - t1
+t1 = datetime.now()
+print(t3)
+print("\n")
+
+print("Quick Sort z wskaźnikiem na środku")
+
+print("sortowanie")
+t1 = datetime.now()
+quickSortMid1(tablica2, 0, dl - 1)
 t2 = datetime.now()
 t3 = t2 - t1
 print(t3)
-# print(tablica2)
 
+print("sortowanie")
+t1 = datetime.now()
+quickSortMid2(tablica2, 0, dl - 1)
+t2 = datetime.now()
+t3 = t2 - t1
+print(t3)
+
+print("sortowanie odwrotne")
 t1 = datetime.now()
 quickSortRand1(tablica3, 0, dl - 1)
-print(tablica3)
-quickSortRand2(tablica3, 0, dl - 1)
-print(tablica3)
 t2 = datetime.now()
 t3 = t2 - t1
 print(t3)
-# print(tablica3)
+print("\n")
 
-os.system("PAUSE")
+print("Quick Sort z wskaźnikiem losowym")
+
+print("sortowanie")
+t1 = datetime.now()
+quickSortRand1(tablica3, 0, dl - 1)
+t2 = datetime.now()
+t3 = t2 - t1
+print(t3)
+
+print("sortowanie")
+t1 = datetime.now()
+quickSortRand1(tablica3, 0, dl - 1)
+t2 = datetime.now()
+t3 = t2 - t1
+print(t3)
+
+print("Sortowanie odwrotne")
+t1 = datetime.now()
+quickSortRand2(tablica3, 0, dl - 1)
+t2 = datetime.now()
+t3 = t2 - t1
+print(t3)
+print("\n")
+
+print("sortowanie heapsport")
+
+print("sortowanie")
+t1 = datetime.now()
+
+t2 = datetime.now()
+t3 = t2 - t1
+print(t3)
+
+print("sortowanie")
+t1 = datetime.now()
+heapSort(tablica4)
+t2 = datetime.now()
+t3 = t2 - t1
+print(t3)
+
+print("sortowanie odwrotne")
+t1 = datetime.now()
+heapSort2(tablica4, n)
+t2 = datetime.now()
+t3 = t2 - t1
+print(t3)
+
